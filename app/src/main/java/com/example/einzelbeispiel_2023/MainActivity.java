0694377e87dfc6c8c0e4f6885bd8d124dc78ac43
Julia Lomonosova <julie.lomonosova283@gmail.com>
2023-03-08 01:21:06 +0100

    Exercise print prime digits is ready

--- app/src/main/java/com/example/einzelbeispiel_2023/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel_2023/MainActivity.java
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,27 +17,67 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        EditText input = findViewById(R.id.Eingabe);
+        TextView output = findViewById(R.id.Ausgabe);
+
         Button btn = findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText input = findViewById(R.id.Eingabe);
-                String matrNumber = input.getText().toString();
-                ClientThread clientThread = new ClientThread(matrNumber);
-                Thread thread = new Thread(clientThread);
-                thread.start();
+        btn.setOnClickListener(view -> {
+            String matrNumber = input.getText().toString();
 
-                try {
-                    thread.join();
-                }
-                catch (InterruptedException ex) {
-                    Log.e("Error", "Interrupted Exception was caught while waiting for a client thread! " + ex.getMessage());
-                }
+            ClientThread clientThread = new ClientThread(matrNumber);
+            Thread thread = new Thread(clientThread);
+            thread.start();
 
-                TextView output = findViewById(R.id.textView2);
-                output.setText(clientThread.getAnswerFromServer());
+            try {
+                thread.join();
             }
+            catch (InterruptedException ex) {
+                Log.e("Error", "Interrupted Exception was caught while waiting for a client thread! " + ex.getMessage());
+            }
+
+            output.setText(clientThread.getAnswerFromServer());
         });
+
+        Button btnPrimeNumbers = findViewById(R.id.primeNumbersBtn);
+        btnPrimeNumbers.setOnClickListener(view -> {
+            String matrNumber = input.getText().toString();
+            //matrNumber has always 8 digits!
+            if (!checkMatrNumber(matrNumber)) {
+                output.setText("Matrikelnummer ist ungueltig!");
+            }
+            else {
+                StringBuilder sb = new StringBuilder();
+                //there are just 4 prime digits - 2,3,5,7
+                int[] primeDigits = {2,3,5,7};
+                int[] matrNumberDigits = new int[8];
+                for (int i = 0; i < matrNumber.length(); i++) {
+                    matrNumberDigits[i] = Character.getNumericValue(matrNumber.charAt(i));
+                }
+
+                for (int i = 0; i < primeDigits.length; i++) {
+                    for (int j = 0; j < matrNumberDigits.length; j++) {
+                        if (matrNumberDigits[j] == primeDigits[i]) {
+                            sb.append(matrNumberDigits[j] + " ");
+                            //when the digits should not be printed twice or more,
+                            //then just put break here
+                        }
+                    }
+                }
+                output.setText(sb.toString());
+            }
+
+        });
+
+    }
+
+    public boolean checkMatrNumber(String matrNumber) {
+        //matrikel Number contains always 8 digits
+        if (matrNumber.length() != 8) {
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 
 }
